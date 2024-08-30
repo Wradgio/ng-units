49e03d419bb3e4e41437f5587e9bcb1ac513f237
Marcel Zúbrik <wradgio@gmail.com>
2024-08-30 13:04:44 +0200

    Issue 67 - exponential value too soon
    1e5 is way too soon when converting meters to millimeters

--- projects/ng-units/src/lib/formatters.ts
+++ projects/ng-units/src/lib/formatters.ts
@@ -5,7 +5,7 @@ export const QuantityFormatters: { [name: string]: QuantityFormatter } = {
     'default': function (value: number): string {
         const abs = Math.abs(value);
         let text: string;
-        if (abs && (abs >= 1e5 || abs <= 1e-2)) {
+        if (abs && (abs >= 1e10 || abs <= 1e-2)) {
             text = removeZeroDigits(value.toExponential(3));
         }
         else {
